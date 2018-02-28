ddbeb64f71c682982632fb73903e9304e26cc81f
JunWang <jstzwj@aliyun.com>
2018-03-01 00:23:04 +0800

    add better titlebar

--- include/octoon/editor/widgets/OTitleBar.h
+++ include/octoon/editor/widgets/OTitleBar.h
@@ -7,10 +7,20 @@
 #include <QMouseEvent>
 #include <QHBoxLayout>
 #include <QApplication>
+#include <QDesktopWidget>
 
-#ifdef Q_OS_WIN
-#	include <qt_windows.h>
-#endif
+enum ResizeRegion
+{
+	Default,
+	North,
+	NorthEast,
+	East,
+	SouthEast,
+	South,
+	SouthWest,
+	West,
+	NorthWest
+};
 
 
 class OTitleBar : public QWidget
@@ -79,18 +89,172 @@ protected:
     // drag and move the window
     virtual void mousePressEvent(QMouseEvent *event) Q_DECL_OVERRIDE
     {
-#ifdef Q_OS_WIN
-		if (ReleaseCapture())
+        if (event->button() == Qt::LeftButton) 
+        {
+            this->m_drag = true;
+            this->dragPos = event->pos();
+            this->resizeDownPos = event->globalPos();
+            this->mouseDownRect = this->rect();
+        }
+    }
+
+    virtual void mouseMoveEvent(QMouseEvent * event)
+    {
+        if (resizeRegion != Default)
+        {
+            handleResize();
+            return;
+        }
+        if(m_move) {
+			this->window()->move(event->globalPos() - dragPos);
+            return;
+        }
+        QPoint clientCursorPos = event->pos();
+        QRect r = this->rect();
+        QRect resizeInnerRect(resizeBorderWidth, resizeBorderWidth, r.width() - 2*resizeBorderWidth, r.height() - 2*resizeBorderWidth);
+        if(r.contains(clientCursorPos) && !resizeInnerRect.contains(clientCursorPos)) {
+            ResizeRegion resizeReg = getResizeRegion(clientCursorPos);
+            setResizeCursor(resizeReg);
+            if (m_drag && (event->buttons() & Qt::LeftButton)) {
+                resizeRegion = resizeReg;
+                handleResize();
+            }
+        }
+        else {
+            setCursor(Qt::ArrowCursor);
+            if (m_drag && (event->buttons() & Qt::LeftButton)) {
+                m_move = true;
+				this->window()->move(event->globalPos() - dragPos);
+            }
+        }
+    }
+
+    void mouseReleaseEvent(QMouseEvent *event)
+    {
+        m_drag = false;
+        if(m_move) {
+            m_move = false;
+            handleMove(event->globalPos());
+        }
+        resizeRegion = Default;
+        setCursor(Qt::ArrowCursor);
+    }
+    void setResizeCursor(ResizeRegion region)
+    {
+        switch (region)
+        {
+        case North:
+        case South:
+            setCursor(Qt::SizeVerCursor);
+            break;
+        case East:
+        case West:
+            setCursor(Qt::SizeHorCursor);
+            break;
+        case NorthWest:
+        case SouthEast:
+            setCursor(Qt::SizeFDiagCursor);
+            break;
+        default:
+            setCursor(Qt::SizeBDiagCursor);
+            break;
+        }
+    }
+    ResizeRegion getResizeRegion(QPoint clientPos)
+    {
+        if (clientPos.y() <= resizeBorderWidth) {
+            if (clientPos.x() <= resizeBorderWidth)
+                return NorthWest;
+            else if (clientPos.x() >= this->width() - resizeBorderWidth)
+                return NorthEast;
+            else
+                return North;
+        }
+        else if (clientPos.y() >= this->height() - resizeBorderWidth) {
+            if (clientPos.x() <= resizeBorderWidth)
+                return SouthWest;
+            else if (clientPos.x() >= this->width() - resizeBorderWidth)
+                return SouthEast;
+            else
+                return South;
+        }
+        else {
+            if (clientPos.x() <= resizeBorderWidth)
+                return West;
+            else
+                return East;
+        }
+    }
+    void handleMove(QPoint pt)
+    {
+		QDesktopWidget* desktop = QApplication::desktop();
+        QPoint currentPos = pt - dragPos;
+        if(currentPos.x()<desktop->screenGeometry().x())
 		{
-			QWidget *pWindow = this->window();
-			if (pWindow->isTopLevel())
+            currentPos.setX(desktop->screenGeometry().x());
+        }
+        else if (currentPos.x()+this->width()>desktop->screenGeometry().x())
+		{
+            currentPos.setX(desktop->screenGeometry().x() - this->width());
+        }
+        if(currentPos.y()<desktop->screenGeometry().y())
+		{
+            currentPos.setY(desktop->screenGeometry().y());
+        }
+		this->window()->move(currentPos);
+    }
+    void handleResize()
+    {
+        int xdiff = QCursor::pos().x() - resizeDownPos.x();
+        int ydiff = QCursor::pos().y() - resizeDownPos.y();
+        switch (resizeRegion)
+        {
+			case East:
 			{
-				SendMessage(HWND(pWindow->winId()), WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0);
+				this->window()->resize(mouseDownRect.width()+xdiff, this->height());
+				break;
 			}
-		}
-		event->ignore();
-#else
-#endif
+			case West:
+			{
+				this->window()->resize(mouseDownRect.width()-xdiff, this->height());
+				this->window()->move(resizeDownPos.x()+xdiff, this->y());
+				break;
+			}
+			case South:
+			{
+				this->window()->resize(this->width(),mouseDownRect.height()+ydiff);
+				break;
+			}
+			case North:
+			{
+				this->window()->resize(this->width(),mouseDownRect.height()-ydiff);
+				this->window()->move(this->x(), resizeDownPos.y()+ydiff);
+				break;
+			}
+			case SouthEast:
+			{
+				this->window()->resize(mouseDownRect.width() + xdiff, mouseDownRect.height() + ydiff);
+				break;
+			}
+			case NorthEast:
+			{
+				this->window()->resize(mouseDownRect.width()+xdiff, mouseDownRect.height()-ydiff);
+				this->window()->move(this->x(), resizeDownPos.y()+ydiff);
+				break;
+			}
+			case NorthWest:
+			{
+				this->window()->resize(mouseDownRect.width()-xdiff, mouseDownRect.height()-ydiff);
+				this->window()->move(resizeDownPos.x()+xdiff, resizeDownPos.y()+ydiff);
+				break;
+			}
+			case SouthWest:
+			{
+				this->window()->resize(mouseDownRect.width()-xdiff, mouseDownRect.height()+ydiff);
+				this->window()->move(resizeDownPos.x()+xdiff, this->y());
+				break;
+			}
+        }
     }
 
     // set title and icon
@@ -179,6 +343,12 @@ private:
     QPushButton *minimizeButton;
     QPushButton *maximizeButton;
     QPushButton *closeButton;
+
+    bool m_drag, m_move;
+    QPoint dragPos, resizeDownPos;
+    const int resizeBorderWidth = 5;
+    ResizeRegion resizeRegion;
+    QRect mouseDownRect;
 };
 
 
