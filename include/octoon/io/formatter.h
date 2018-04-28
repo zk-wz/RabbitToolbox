ae787ed90e4ee3c37f4556e08cb4a9f059f05ea8
JunWang <1103870790@qq.com>
2018-04-29 02:06:20 +0800

    add formatter

--- include/octoon/io/formatter.h
+++ include/octoon/io/formatter.h
@@ -17,30 +17,30 @@ namespace octoon
         class Formatter
         {
         public:
-            virtual void deserialize(istream& stream, bool& v) except = 0;
-            virtual void deserialize(istream& stream, float& v) except = 0;
-            virtual void deserialize(istream& stream, double& v) except = 0;
-            virtual void deserialize(istream& stream, char& v) except = 0;
-            virtual void deserialize(istream& stream, unsigned char& v) except = 0;
-            virtual void deserialize(istream& stream, std::int16_t& v) except = 0;
-            virtual void deserialize(istream& stream, std::int32_t& v) except = 0;
-            virtual void deserialize(istream& stream, std::int64_t& v) except = 0;
-            virtual void deserialize(istream& stream, std::uint16_t& v) except = 0;
-            virtual void deserialize(istream& stream, std::uint32_t& v) except = 0;
-            virtual void deserialize(istream& stream, std::uint64_t& v) except = 0;
+            virtual void deserialize(istream& stream, const std::string &name, bool& v) except = 0;
+            virtual void deserialize(istream& stream, const std::string &name, float& v) except = 0;
+            virtual void deserialize(istream& stream, const std::string &name, double& v) except = 0;
+            virtual void deserialize(istream& stream, const std::string &name, char& v) except = 0;
+            virtual void deserialize(istream& stream, const std::string &name, unsigned char& v) except = 0;
+            virtual void deserialize(istream& stream, const std::string &name, std::int16_t& v) except = 0;
+            virtual void deserialize(istream& stream, const std::string &name, std::int32_t& v) except = 0;
+            virtual void deserialize(istream& stream, const std::string &name, std::int64_t& v) except = 0;
+            virtual void deserialize(istream& stream, const std::string &name, std::uint16_t& v) except = 0;
+            virtual void deserialize(istream& stream, const std::string &name, std::uint32_t& v) except = 0;
+            virtual void deserialize(istream& stream, const std::string &name, std::uint64_t& v) except = 0;
 
 
-            virtual void serialize(ostream& stream, bool v) except = 0;
-            virtual void serialize(ostream& stream, float v) except = 0;
-            virtual void serialize(ostream& stream, double v) except = 0;
-            virtual void serialize(ostream& stream, char v) except = 0;
-            virtual void serialize(ostream& stream, unsigned char v) except = 0;
-            virtual void serialize(ostream& stream, std::int16_t v) except = 0;
-            virtual void serialize(ostream& stream, std::int32_t v) except = 0;
-            virtual void serialize(ostream& stream, std::int64_t v) except = 0;
-            virtual void serialize(ostream& stream, std::uint16_t v) except = 0;
-            virtual void serialize(ostream& stream, std::uint32_t v) except = 0;
-            virtual void serialize(ostream& stream, std::uint64_t v) except = 0;
+            virtual void serialize(ostream& stream, const std::string &name, bool v) except = 0;
+            virtual void serialize(ostream& stream, const std::string &name, float v) except = 0;
+            virtual void serialize(ostream& stream, const std::string &name, double v) except = 0;
+            virtual void serialize(ostream& stream, const std::string &name, char v) except = 0;
+            virtual void serialize(ostream& stream, const std::string &name, unsigned char v) except = 0;
+            virtual void serialize(ostream& stream, const std::string &name, std::int16_t v) except = 0;
+            virtual void serialize(ostream& stream, const std::string &name, std::int32_t v) except = 0;
+            virtual void serialize(ostream& stream, const std::string &name, std::int64_t v) except = 0;
+            virtual void serialize(ostream& stream, const std::string &name, std::uint16_t v) except = 0;
+            virtual void serialize(ostream& stream, const std::string &name, std::uint32_t v) except = 0;
+            virtual void serialize(ostream& stream, const std::string &name, std::uint64_t v) except = 0;
         };
     }
 }
